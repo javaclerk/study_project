29556c85a3177f9802fadae887e53b5eda038752
javaclerk <chg5638@gmail.com>
2023-01-20 08:55:08 +0900

    cmake update

--- gps_test/CMakeFiles/3.19.2/CMakeSystem.cmake
+++ gps_test/CMakeFiles/3.19.2/CMakeSystem.cmake
@@ -3,13 +3,13 @@ set(CMAKE_HOST_SYSTEM_NAME "Linux")
 set(CMAKE_HOST_SYSTEM_VERSION "4.15.0-197-generic")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
-include("/opt/fsl-imx-xwayland/4.14-thud/sysroots/x86_64-pokysdk-linux/usr/share/cmake/OEToolchainConfig.cmake")
 
-set(CMAKE_SYSTEM "Linux")
+
+set(CMAKE_SYSTEM "Linux-4.15.0-197-generic")
 set(CMAKE_SYSTEM_NAME "Linux")
-set(CMAKE_SYSTEM_VERSION "")
-set(CMAKE_SYSTEM_PROCESSOR "aarch64")
+set(CMAKE_SYSTEM_VERSION "4.15.0-197-generic")
+set(CMAKE_SYSTEM_PROCESSOR "x86_64")
 
-set(CMAKE_CROSSCOMPILING "TRUE")
+set(CMAKE_CROSSCOMPILING "FALSE")
 
 set(CMAKE_SYSTEM_LOADED 1)
