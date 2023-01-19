02d8e061c0946e808c0134b0e3125633e8a6dd58
javaclerk <chg5638@gmail.com>
2023-01-19 14:25:03 +0900

    study_project_upload

---
+++ msgq_test/receiver/CMakeFiles/program_receiver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/program_receiver.dir/main_receiver.c.o"
+  "program_receiver.pdb"
+  "program_receiver"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/program_receiver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ msgq_test/sender/CMakeFiles/program_sender.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/program_sender.dir/main_sender.c.o"
+  "program_sender.pdb"
+  "program_sender"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/program_sender.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
