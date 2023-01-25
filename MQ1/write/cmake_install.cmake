5c11ff1e6bbebc6789aa2e97b846e57c4510cb21
javaclerk <chg5638@gmail.com>
2023-01-25 10:44:32 +0900

    asdf

--- MQ1/write/cmake_install.cmake
+++ MQ1/write/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/chj/workspace/study_project/msgq_test/receiver
+# Install script for directory: /home/chj/workspace/study_project/MQ1/write
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,12 @@ endif()
 
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
-  set(CMAKE_CROSSCOMPILING "TRUE")
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/opt/fsl-imx-xwayland/4.14-thud/sysroots/x86_64-pokysdk-linux/usr/bin/aarch64-poky-linux/aarch64-poky-linux-objdump")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/chj/workspace/study_project/msgq_test/receiver/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/chj/workspace/study_project/MQ1/write/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
