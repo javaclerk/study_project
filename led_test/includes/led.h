357d49ca4cd72a40e0780245395ef299becbb279
javaclerk <chg5638@gmail.com>
2023-01-26 08:28:20 +0900

    asfd

--- led_test/includes/led.h
+++ led_test/includes/led.h
@@ -1,11 +1,9 @@
-int i;
+class Led_OFF{
+    public:
 
-if(ledSetup()==-1)retunr -1;
+};
 
-for(i=0;i<5;i++){
-    digitalWrite(29,1);
-    delay(500);
-    digitalWrite(29,0);
-    delay(500);
-}
-return 0;
+class Led_ON{
+    public:
+    
+};
